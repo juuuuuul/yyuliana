467b931e65f2ee4f2d189e08dbf8f83a944fa492
juuuuuul <113307687+juuuuuul@users.noreply.github.com>
2022-09-29 18:58:18 +0300

    Update task1.py

--- task1.py
+++ task1.py
@@ -1,16 +1,16 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-import math
-import numpy
-import matplotlib.pyplot as mpp
+import math  # импорт из математической библиотеки
+import numpy  # импорт из библиотеки
+import matplotlib.pyplot as mpp  # импорт из библиотеки в Python project
 
 # Эта программа рисует график функции, заданной выражением ниже
 
-if __name__=='__main__':
-    arguments = numpy.arange(0, 200, 0.1)
-    mpp.plot(
+if __name__=='__main__':  # питон проверяет и запускает файлы
+    arguments = numpy.arange(0, 200, 0.1)  # задаёт координаты для графика
+    mpp.plot(  # строит график
         arguments,
-        [math.sin(a) * math.sin(a/20.0) for a in arguments]
+        [math.sin(a) * math.sin(a/20.0) for a in arguments]  # задаёт функцию
     )
-    mpp.show()
+    mpp.show()  # выводит график
